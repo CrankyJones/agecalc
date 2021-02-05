7e06968c65ce6d5c0514850f0cd2e00f9501f666
Geof Rosenmund <geof.rosenmunds.email@gmail.com>
2021-02-05 08:38:04 -0800

    reread prompt and start devising a strategy, create js and testfolders and files

---
+++ __tests__/calc.tests.js
@@ -0,0 +1,13 @@
+import { TestScheduler } from 'jest';
+import xxxxxx from '../src/calc.js';
+
+describe('Calculator', () => {
+
+
+  test('Should return the age submitted into Mercury equivalent', () {
+    const age = 31;
+    expect(calc.merc(age)).toEqual()
+
+
+  })
+})
