197c5b488717005b4eac16408f1bf709fc49f045
Geof Rosenmund <geof.rosenmunds.email@gmail.com>
2021-02-05 09:56:30 -0800

    Mercury test 2 good fail

--- __tests__/calc.tests.js
+++ __tests__/calc.tests.js
@@ -5,7 +5,7 @@ describe('Calculator', () => {
 
   let newAge;
   beforeEach(() => {
-    newAge = new Calc(30);
+    newAge = new Calc(30 , 70);
   })
 
   test('Should receive an input and multiply that by earth days', () => {
@@ -28,5 +28,9 @@ describe('Calculator', () => {
   
   test('Should return the age submitted into Jupiter equivalent', () => {
     expect(newAge.jupi()).toEqual(2);
+  })
+
+  test('Should take age and life expectancy and convert into Mercury equivalent', () => {
+    expect(newAge.merc()).toEqual("Your age on Mercury would be 124, and you have 166 years left to live.")
   })
 })
