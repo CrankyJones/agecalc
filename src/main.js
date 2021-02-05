4b75955e2fbeba7f09d6b6982ff25b949f468e28
Geof Rosenmund <geof.rosenmunds.email@gmail.com>
2021-02-05 13:47:42 -0800

    Work on setting up main.js and index.html

---
+++ src/main.js
@@ -0,0 +1,7 @@
+import {Calc , Mercury, Venus, Mars, Jupiter} from './js/calc.js';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './css/styles.css';
+import $ from 'jquery';
+
+$(document).ready
