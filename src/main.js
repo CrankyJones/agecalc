e2c8831dbc66ecf32a4e78bf40c7a7e942311652
Geof Rosenmund <geof.rosenmunds.email@gmail.com>
2021-02-05 15:46:55 -0800

    Work on getting away from an alert box and towards an on screen box

--- src/main.js
+++ src/main.js
@@ -1,49 +1,53 @@
-import { Calc, Mercury, Venus, Mars, Jupiter } from './js/calc.js';
+import { Mercury, Venus, Mars, Jupiter } from './js/calc.js';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import $ from 'jquery';
 
+
+
+document.getElementById("mercury").onclick = function () {
+  const age = $("#age").val();
+  const expect = $("#expect").val();
+  const newAge = new Mercury(age, expect);
+  const results = newAge.ageConv();
+  $("#results").text(results);
+  $(".resultsWindow").show;
+  document.getElementById("form").reset();
+};
+
+document.getElementById("venus").onclick = function () {
+  const age = $("#age").val();
+  const expect = $("#expect").val();
+  const newAge = new Venus(age, expect);
+  const results = newAge.ageConv();
+  $("#results").text(results);
+  $(".resultsWindow").show;
+  document.getElementById("form").reset();
+};
+
+document.getElementById("mars").onclick = function () {
+  const age = $("#age").val();
+  const expect = $("#expect").val();
+  const newAge = new Mars(age, expect);
+  const results = newAge.ageConv();
+  $("#results").text(results);
+  $(".resultsWindow").show;
+  document.getElementById("form").reset();
+};
+
+document.getElementById("jupiter").onclick = function () {
+  const age = $("#age").val();
+  const expect = $("#expect").val();
+  const newAge = new Jupiter(age, expect);
+  const results = newAge.ageConv();
+  $(".results").text(results);
+  $(".resultsWindow").show;
+  document.getElementById("form").reset();
+};
+
 $(document).ready(function () {
-
-  document.getElementById("mercury").onclick = function () {
-    const age = $("#age").val();
-    const expect = $("#expect").val();
-    const newAge = new Mercury(age, expect);
-    const results = newAge.ageConv();
-    $("#results").text(results);
-    $(".resultsWindow").show;
-    document.getElementById("form").reset();
-  };
-
-  document.getElementById("venus").onclick = function () {
-    const age = $("#age").val();
-    const expect = $("#expect").val();
-    const newAge = new Venus(age, expect);
-    const results = newAge.ageConv();
-    $("#results").text(results);
-    $(".resultsWindow").show;
-    document.getElementById("form").reset();
-  };
-
-  document.getElementById("mars").onclick = function () {
-    const age = $("#age").val();
-    const expect = $("#expect").val();
-    const newAge = new Mars(age, expect);
-    const results = newAge.ageConv();
-    $("#results").text(results);
-    $(".resultsWindow").show;
-    document.getElementById("form").reset();
-  };
-
-  document.getElementById("jupiter").onclick = function () {
-    const age = $("#age").val();
-    const expect = $("#expect").val();
-    const newAge = new Jupiter(age, expect);
-    const results = newAge.ageConv();
-    $("#results").text(results);
-    $(".resultsWindow").show;
-    document.getElementById("form").reset();
-  };
-
+  $("#form").submit(function (event) {
+    event.preventDefault();
+  });
 });
