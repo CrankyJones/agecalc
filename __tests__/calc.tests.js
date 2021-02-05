7ef913ebbebbc7022f35acc06c316ab5e4d23e63
Geof Rosenmund <geof.rosenmunds.email@gmail.com>
2021-02-05 10:38:32 -0800

    Multiple Planet expectancy conversion test 1 good fail

--- __tests__/calc.tests.js
+++ __tests__/calc.tests.js
@@ -18,25 +18,24 @@ describe('Calculator', () => {
     expect(newAge.mercAge).toEqual(124);
   })
 
-  test('Should return the age submitted into the Venus equivalent', () => {
+  test('Should return the age submitted into the Venus, Mars, & Jupiter equivalent', () => {
     newAge.venu();
-    expect(newAge.venuAge).toEqual(48);
-  })
-
-  test('Should return the age submitted into Mars equivalent', () => {
     newAge.mars();
+    newAge.jupi();
+    expect(newAge.venuAge).toEqual(48);
     expect(newAge.marsAge).toEqual(15);
-    
-  })
-  
-  test('Should return the age submitted into Jupiter equivalent', () => {
-    newAge.jupi()
     expect(newAge.jupiAge).toEqual(2);
   })
 
   test('Should take age and life expectancy and convert into Mercury equivalent', () => {
     expect(newAge.merc()).toEqual("Your age on Mercury would be 124, and you have 166 years left to live.")
   })
-  
+
+  test('Should take age and life expectancy and convert into Venus, Mars, & Jupiter equivalent', () => {
+    expect(newAge.venu()).toEqual("Your age on Mercury would be 48, and you have 112 years left to live.")
+    expect(newAge.mars()).toEqual("Your age on Mars would be 15, and you have 37 years left to live.")
+    expect(newAge.jupi()).toEqual("Your age on Jupiter would be 2, and you have 5 years left to live.")
+  })
+
 
 })
