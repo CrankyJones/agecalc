38a330af976b839498dcd5e310aae9fd04774038
Geof Rosenmund <geof.rosenmunds.email@gmail.com>
2021-02-05 14:32:50 -0800

    Attempt to get a button clicking selector working

--- src/main.js
+++ src/main.js
@@ -5,11 +5,22 @@ import './css/styles.css';
 import $ from 'jquery';
 
 
+
+
+
+
+
 $(document).ready(function(){
   $("#form").click(function(event){
     event.preventDefault();
-    
+    const age = $("#age").val();
+    const expect = $("#expect").val();
 
+    if ($( "#mercury" ).click()) {
+      const newAge = new Mercury(age, expect);
+      const results = newAge.ageConv();
+      alert(results);
+    }
 
-  })
-})
+  });
+});
